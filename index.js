b149fed7d1787ec9dbf7e25bb26188f5f855e981
Viitorsoares <viitorgab.soares@gmail.com>
2024-01-27 04:37:56 -0300

    Jogo da velha Javascript
    Atualizando o javascript

--- index.js
+++ index.js
@@ -10,4 +10,36 @@ function updateTitle() {
 function initializeGame() {
     vBoard = [['','',''],['','',''],['','','']]
     turnPlayer = 'player1'
+    document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
+    updateTitle()
+    boardRegions.forEach(function (element) { 
+        element.classList.remove('win')
+        element.innerText = ''
+        element.addEventListener('click', handleBoardClick)
+    })
 }
+
+function disableRegion(element) {
+    element.style.cursor = 'default'
+    element.removeEventListener('click', handleBoardClick)
+}
+
+function handleBoardClick(ev) {
+    const span = ev.currentTarget
+    const region = ev.currentTarget.dataset.region
+    const rowColumnPair = region.split('.')
+    const row = rowColumnPair[0]
+    const column = rowColumnPair[1]
+    if (turnPlayer === 'player1') {
+        span.innerText = 'X'
+        vBoard[row][column] = 'X'
+    } else {
+        span.innerText = 'O'
+        vBoard[row][column] = 'O'
+    }
+    console.clear()
+    console.table(vBoard)
+    disableRegion(span)
+}
+
+document.getElementById('start').addEventListener('click', initializeGame)
